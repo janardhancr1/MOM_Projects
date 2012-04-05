e50655b8b3a20b42ed91ea7e2717db35b2a2e80b
keshavanarayanamc@gmail.com <keshavanarayanamc@gmail.com@c7d04806-0fe9-44f9-b1d5-8765aa47b377>
2012-04-05 16:42:33 +0000

    Image changes

--- CarDriver/App/CarDriver/application/forms/Reviewedit.php
+++ CarDriver/App/CarDriver/application/forms/Reviewedit.php
@@ -4300,7 +4300,8 @@ class Application_Form_Reviewedit extends Application_Form_MainForm
 			
 			$before_image = new Zend_Form_Element_Image('image');
 			if($rt_results_level_3)
-				$before_image->setImage($rt_results_level_3[0]['image']);
+				$before_image->setImage('C:\wamp\tmp/')
+						 ->setImageValue($rt_results_level_3[0]['image']);
 			
 			
 			$image->setDecorators(array(
